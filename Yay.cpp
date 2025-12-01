01136990d9d88c652bdb0832e302d168dc645b72
sd1827 <sharvaridhile@gmail.com>
2025-12-01 14:26:37 -0500

    Added stuff

--- Yay.cpp
+++ Yay.cpp
@@ -1,1 +1,18 @@
 // YYAAAAAAYYYYYYYY
+
+#include "FEHLCD.h"
+
+int main()
+{
+    LCD.WriteLine("Hello World!");
+    LCD.WriteLine("Menu");
+    LCD.WriteLine("Play Game");
+    LCD.WriteLine("Statistics");
+    LCD.WriteLine("Instructions");
+    LCD.WriteLine("Credits");
+    while (1) {
+        LCD.Update();
+        // Never end
+    }
+    return 0;
+}
