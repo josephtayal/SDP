7bb36247ff5a1c0f612d80d935d3b491010c8de8
sd1827 <sharvaridhile@gmail.com>
2025-12-01 14:36:42 -0500

    Started menu

--- Yay.cpp
+++ Yay.cpp
@@ -2,17 +2,33 @@
 
 #include "FEHLCD.h"
 
+void PlayGame() {
+    LCD.WriteLine("Play game here");
+}
+
+void Menu() {
+
+}
+
 int main()
 {
-    LCD.WriteLine("Hello World!");
+    int x, y;
+
     LCD.WriteLine("Menu");
     LCD.WriteLine("Play Game");
     LCD.WriteLine("Statistics");
     LCD.WriteLine("Instructions");
     LCD.WriteLine("Credits");
-    while (1) {
-        LCD.Update();
-        // Never end
-    }
-    return 0;
+
+    // Wait for touch - use x and y
+    while (!LCD.Touch(&x,&y)) {}
+
+    // Touch - use x and y
+    while (LCD.Touch(&x,&y)) {}
+
+    
+
+
+
+    
 }
