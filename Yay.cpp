e1933017fdbbde5fb14d44336d89e58d8afcd7b5
sd1827 <sharvaridhile@gmail.com>
2025-12-01 20:29:50 -0500

    Update Yay.cpp

--- Yay.cpp
+++ Yay.cpp
@@ -1,33 +1,40 @@
 // YYAAAAAAYYYYYYYY
 
 #include "FEHLCD.h"
+#include "FEHUtility.h"
+#include "FEHImages.h"
 
 void PlayGame() {
-    LCD.WriteLine("Play game here");
-}
-
-void Menu() {
-
-}
+    LCD.Clear();
+    LCD.Write("Play the game");
+} 
 
 int main()
 {
-    int x, y;
+    int x_position, y_position;
+    int x_trash, y_trash;
 
-    LCD.WriteLine("Menu");
-    LCD.WriteLine("Play Game");
-    LCD.WriteLine("Statistics");
-    LCD.WriteLine("Instructions");
-    LCD.WriteLine("Credits");
+    LCD.DrawRectangle(50, 30, 220, 30);
+    LCD.WriteAt("Play Game", 85, 35);
+    
+    LCD.DrawRectangle(50, 60, 220, 30);
+    LCD.WriteAt("Statistics", 85, 65);
+    
+    LCD.DrawRectangle(50, 90, 220, 30);
+    LCD.WriteAt("Instructions", 85, 95);
+    
+    LCD.DrawRectangle(50, 120, 220, 30);
+    LCD.WriteAt("Credits", 85, 125);
 
     // Wait for touch - use x and y
-    while (!LCD.Touch(&x,&y)) {}
+    while (!LCD.Touch(&x_position,&y_position)) {}
 
     // Touch - use x and y
-    while (LCD.Touch(&x,&y)) {}
+    while (LCD.Touch(&x_trash,&y_trash)) {}
 
-    
-
+    if (x_position >= 50 && x_position <= 270 && y_position >= 30 && y_position <= 60) {
+        PlayGame();
+    }
 
 
     
