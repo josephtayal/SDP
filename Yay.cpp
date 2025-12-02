3d0a846be00cfa547a75b995218e8e9b28ed430e
sd1827 <sharvaridhile@gmail.com>
2025-12-02 14:16:30 -0500

    Update Yay.cpp

--- Yay.cpp
+++ Yay.cpp
@@ -11,6 +11,10 @@ void Stats();
 void Instructions();
 void Credits();
 void PlayGame();
+void LevelOne();
+void LevelTwo();
+void ChooseLevel();
+void DrawBoard();
 
 void BackToMenu() {
     int x_position, y_position;
@@ -25,21 +29,65 @@ void BackToMenu() {
         while (LCD.Touch(&x_trash,&y_trash)) {}
 
     // Brings user back to the main menu
-        if (x_position >= 50 && x_position <= 270 && y_position >= 200 && y_position <= 250) {
+        if (x_position >= 115 && x_position <= 270 && y_position >= 200 && y_position <= 250) {
             LCD.Clear();
             Menu();
         } 
     }
 }
 
+void ChooseLevel() {
+    int x_position, y_position;
+    int x_trash, y_trash;
+
+    // Waits for the user to touch the screen
+    while(true) {
+        // Wait for touch - use x and y
+        while (!LCD.Touch(&x_position,&y_position)) {}
+
+        // Touch - use x and y
+        while (LCD.Touch(&x_trash,&y_trash)) {}
+
+    // Brings user to specific level
+        if (x_position >= 95 && x_position <= 155 && y_position >= 95 && y_position <= 155) {
+            LevelOne();
+        } else if (x_position >= 165 && x_position <= 235 && y_position >= 95 && y_position <= 155) {
+            LCD.Clear();
+        }
+    }
+}
+
+void DrawBoard() {
+    LCD.Clear();
+    LCD.SetBackgroundColor(BURLYWOOD);
+}
+
+
+void LevelOne() {
+    LCD.Clear();
+    LCD.SetBackgroundColor(BURLYWOOD);
+}
+
+/*
+void LevelTwo() {
+    // DrawBoard function
+    // Other draw functions
+} 
+*/
+
 void PlayGame() {
     // Displays the screen to play the game
     LCD.Clear(BLACK);
     LCD.Update();
-    LCD.WriteLine("Play the game");
-    LCD.WriteAt("Main Menu", 50, 200);
+    LCD.WriteAt("Choose Level", 85, 30);
+    LCD.DrawCircle(120, 120, 35);
+    LCD.DrawCircle(200, 120, 35);
+    LCD.WriteAt("1", 115, 110);
+    LCD.WriteAt("2", 195, 110);
+    LCD.WriteAt("Main Menu", 115, 200);
     LCD.Update();
 
+    ChooseLevel();
     BackToMenu();
     LCD.Update();
 
@@ -52,7 +100,7 @@ void Stats() {
     LCD.WriteLine("Games Played: ");
     LCD.WriteLine("Wins: ");
     LCD.WriteLine("Losses: ");
-    LCD.WriteAt("Main Menu", 50, 200);
+    LCD.WriteAt("Main Menu", 115, 200);
     LCD.Update();
 
     BackToMenu();
@@ -64,7 +112,7 @@ void Instructions () {
     LCD.Clear(BLACK);
     LCD.Update();
     LCD.Write("Cut the Rope is a simple game. The goal is to cut the rope that is connected to the candy and make sure the creature eats the candy.");
-    LCD.WriteAt("Main Menu", 50, 200);
+    LCD.WriteAt("Main Menu", 115, 200);
     LCD.Update();
 
     BackToMenu();
@@ -78,7 +126,7 @@ void Credits () {
     LCD.Write("Developers: ");
     LCD.Write("Joe Tayal");
     LCD.Write("Sharvari Dhile");
-    LCD.WriteAt("Main Menu", 50, 200);
+    LCD.WriteAt("Main Menu", 115, 200);
     LCD.Update();
 
     BackToMenu();
